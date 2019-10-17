07b6064cb31140eab5272a09dc1e009251768703
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-17 14:36:17 -0600

    react setup and dependencies installed

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Forms from './Forms';
+
+function App() {
+  return (
+    <div className="App">
+      <Forms />
+    </div>
+  );
+}
+
+export default App;
